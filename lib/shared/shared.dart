ff6b8d2dfb106ec8c7166586df9d2c969046b3a9
“dwianaambarr” <dwianaambarr@gmail.com>
2020-12-11 00:44:12 +0700

    add splash screen

--- lib/shared/shared.dart
+++ lib/shared/shared.dart
@@ -1,5 +1,8 @@
 import 'package:flutix/bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 part 'shared_value.dart';
+part 'theme.dart';
 
 
